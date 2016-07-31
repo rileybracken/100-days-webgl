1d780764a463feab832175ed562127913b3491d7
Riley Bracken <rileybracken@gmail.com>
2016-07-30 20:16:15 -0600

    Adds day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import D001 from './d001.js';
+// import D001 from './d001.js';
+import D002 from './d002.js';
 
 class App extends Component {
   render() {
-    return <D001 />;
+    return <D002 />;
   }
 }
 
--- src/utils/gl.js
+++ src/utils/gl.js
@@ -41,3 +41,57 @@ export function createProgram(gl, vertexShader, fragmentShader) {
 
   gl.deleteProgram(program);
 }
+
+export function setRectangle(gl, x, y, w, h) {
+  let x1 = x;
+  let x2 = x + w;
+  let y1 = y;
+  let y2 = y + h;
+
+  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+  // but so far we only have one buffer. If we had more than one
+  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    x1, y1,
+    x2, y1,
+    x1, y2,
+    x1, y2,
+    x2, y1,
+    x2, y2,
+  ]),
+  gl.STATIC_DRAW);
+}
+
+export function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      // left column
+      0, 0,
+      30, 0,
+      0, 150,
+      0, 150,
+      30, 0,
+      30, 150,
+
+      // top rung
+      30, 0,
+      100, 0,
+      30, 30,
+      30, 30,
+      100, 0,
+      100, 30,
+
+      // middle rung
+      30, 60,
+      67, 60,
+      30, 90,
+      30, 90,
+      67, 60,
+      67, 90,
+    ]),
+    gl.STATIC_DRAW
+  );
+}
